35f995cc11185649d0d3fe6ef2f6826f9be1d5a0
Vinicius de Santana <vinicius.vts@gmail.com>
2021-02-24 21:45:51 -0300

    fix: change footer layout

--- wp-content/themes/ssw/template-parts/footer-main.php
+++ wp-content/themes/ssw/template-parts/footer-main.php
@@ -1,17 +1,29 @@
 <footer class="bg-p-dark">
     <div class="container">
         <div class="row">
-            <div class="col-4">
+            <div class="col-md-4 c-1">
+                <h2>Santana Soluções Web</h2>
+                <p>Somos uma empresa especializada em criação de sites.
+                    Obtendo resultados efetivos com excelente atendimento.</p>
+            </div>
+            <div class="col-md-2 offset-md-1 c-2">
                 <a class="d-flex" href="/">
                     <?php echo(file_get_contents(get_template_directory() . '/assets/img/sswLogowhite.svg')); ?>
                 </a>
-                <h2>Santana Soluções Web</h2>
-                <p>Somos uma empresa especializada em criação de sites.
-                    Obtendo resultados efetivos desde 2019</p>
             </div>
-            <div class="col-4 offset-2">
-                <h2>Onde estamos:</h2>
-                <p>Trv Carlos Fraga n° 38, Salvador Bahia Brasil.</p>
+            <div class="col-md-4 offset-md-1 c-3">
+                <h2>Principais links</h2>
+                <ul class="nav">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/blog/">Blog</a>
+                    </li>
+                    <li>
+                        <a href="/contato/">Contato</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
