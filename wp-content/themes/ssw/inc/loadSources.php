bf0d8851addd287ba645aa0d4a5c67e62fc0055d
Vinicius de Santana <vinicius.vts@gmail.com>
2021-03-31 23:02:09 -0300

    feat: add debug source load

--- wp-content/themes/ssw/inc/loadSources.php
+++ wp-content/themes/ssw/inc/loadSources.php
@@ -12,6 +12,44 @@ function add_css_and_js() {
   $jsInternalPath = get_template_directory() . "/"."assets/js/";
   $jsUriPath = get_template_directory_uri() . "/"."assets/js/";
 
+  $archive = 'script.min.js';
+  $urlPath = $jsUriPath . $archive;
+  $internalPath = $jsInternalPath . $archive;
+  $fileVersion = filemtime($internalPath);
+  wp_enqueue_script( $archive, $urlPath, array (), $fileVersion, true);
+  
+  //###############################################################################################
+  //styles: wp_enqueue_style( $nome, $origem, $dependencia, $versao, $media );
+
+  $media = 'all';
+  $cssInternalPath = get_template_directory() . "/"."assets/css/";
+  $cssUriPath = get_template_directory_uri() . "/"."assets/css/";
+
+  $archive = 'style.css';
+  $urlPath = $cssUriPath . $archive;
+  $internalPath = $cssInternalPath . $archive;
+  $fileVersion = filemtime($internalPath);
+  wp_enqueue_style( $archive, $urlPath, array(), $fileVersion, $media );
+  // Poppins fonte
+  wp_enqueue_style( 'poppinFont',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap',
+    array()
+  );
+}
+
+/**
+ * Adiciona os scripts e folha de estilos ao site, modo de debug
+ *
+ * @package SSW
+ * @subpackage loadSources
+ * @author Vinicius de Santana
+ */
+function add_css_and_js_debug() {
+  //scripts: wp_enqueue_script( $nome, $origem, $dependencia, $versao, $rodape );
+
+  $jsInternalPath = get_template_directory() . "/"."assets/js/";
+  $jsUriPath = get_template_directory_uri() . "/"."assets/js/";
+
   $archive = 'script.js';
   $urlPath = $jsUriPath . $archive;
   $internalPath = $jsInternalPath . $archive;
@@ -36,8 +74,12 @@ function add_css_and_js() {
     array()
   );
 }
-//do it
-add_action( 'wp_enqueue_scripts', 'add_css_and_js' );
+//do it if debug
+if(WP_DEBUG){
+  add_action( 'wp_enqueue_scripts', 'add_css_and_js_debug' );
+} else{
+  add_action( 'wp_enqueue_scripts', 'add_css_and_js' );
+}
 
 //examples
 /*
